997605a889d28ac4f96589f26d57701a383a554d
Marvelous E. Azubuike <azubuikemarvellous14@gmail.com>
2023-12-02 05:13:26 +1100

    did some of my assignment

---
+++ main.js
@@ -0,0 +1,149 @@
+//DATATYPES PRACTICE QUESTIONS
+
+1.
+// const challenge = 'Pluralcode February Cohort Javascript Classes' ;
+
+2.
+// alert (challenge);
+
+3.
+// const theSaying = " The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+// console.log(theSaying)
+// console.log(typeof theSaying)
+
+4.
+// const a = '10';
+// const b = 10;
+//console.log(typeof a, typeof b)...//not equal
+
+//console.log(typeof parseInt(a) , typeof b); //I called (a) using parseInt to change it to an Number
+
+5.
+//
+
+6.
+// const a = ('My name is ' + 'web dev');
+// console.log(a)
+
+7.
+// const name = 'Marvelous';
+// const age = '25 yrs old';
+// const shortDescription = 'Web dev student in pluralcode';
+// const faveWebDevTopic = 'Datatypes'
+// const info = 'My name is ' + name + ',I am ' + age + ',I study as a ' + shortDescription + '. my favorite topic is ' + faveWebDevTopic;
+// alert(info);
+
+
+
+//CONDITIONALS PRACTICE QUESTIONS
+
+1.
+
+
+
+
+
+
+
+
+
+
+//OPERATORS PRACTICE QUESTIONS
+
+1.
+// const firstName = "Marvelous";
+// const lastName = "Azubuike";
+// const country = "Nigeria";
+// const city = "Lagos City";
+// const age = 25;
+// const isMarried = false;
+// const year = 1998;
+
+// console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof age, typeof isMarried, typeof year)
+
+//Answer : string string string string number boolean number
+
+2.
+// const a = '10';
+// const b = 10;
+// console.log(typeof a, typeof b)
+//typeof (a) is a string while typeof (b) is a number
+
+
+3.
+
+4.
+// TRUTHY VALUES
+
+//a.
+// const a = '200';
+// const b = 200;
+// const c = a == b;..TRUE
+// console.log(c)...this is TRUE because LOOSE COMPARISON changes the datatypes to the same as long as the values are the same.
+
+//b.
+// const d = 'Adamu';
+// const e = 'Adamu';
+// const f = d === e;..TRUE
+// console.log(f)....this is TRUE because the datatype and values are the same which matches the law of STRICT COMPARISON(===)
+
+//c.
+// const a = 24;
+// const b = 24;
+// const c = a == b;...TRUE
+// console.log(c)...this is TRUE because both the values and datatype are the same
+
+//FALSY VALUES
+
+//a.
+// const a = '200';
+// const b = 200;
+// const c = a === b;...FALSE
+// console.log(c)....this is FALSE because the datatype are not the same which does not match with the law of STRICT COMPARISON(===)
+
+//b.
+// const d = 'Adamu';
+// const e = 'Musa';
+// const f = d === e;
+// console.log(f)...this is FALSE because the values are not the same which does not match with the law of STRICT COMPARISON(===)
+
+//c.
+// const a = '200';
+// const b = '100';
+// const c = a == b;....FALSE
+// console.log(c)...this is FALSE because the values are not the same which does not match with the law of LOOSE COMPARISON(==)
+
+5.
+4 > 3 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
